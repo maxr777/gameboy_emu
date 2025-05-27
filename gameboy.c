afcc4d6c2944c8db69bb5349e42440058506d8fb
max <maxr.0307@gmail.com>
2025-05-27 20:50:52 +0200

    Define functions for load instructions

--- gameboy.c
+++ gameboy.c
@@ -6,6 +6,7 @@ union Register regs[REGISTER_COUNT] = {0};
 bool display[160][144] = {0};
 uint8_t ram[8192] = {0};
 uint8_t vram[8192] = {0};
+int cycle = 0;
 
 void set_zero_flag(bool f) {
   if (f)
@@ -29,7 +30,6 @@ void set_half_carry_flag(bool f) {
 }
 
 void set_carry_flag(bool f) {
-
   if (f)
     regs[AF].low |= 0x10;
   else
