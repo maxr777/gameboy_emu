343fbf84eeac2d7d25dd039d0854c0df64d41d06
Скибидислав Тоилетов <maxr.0307@gmail.com>
2025-06-14 00:34:31 +0200

    Copy the .dll file to the main folder instead (on windows)

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 @echo off
 cl /I"SDL3-win\include" main.c gameboy.c /link /LIBPATH:"SDL3-win\lib\x86" SDL3.lib /out:a.exe
-if %errorlevel% == 0 set "PATH=%PATH%;SDL3-win\lib\x86"
-if %errorlevel% == 0 a.exe cpu_instrs.gb
+if %errorlevel% == 0 (
+    if not exist SDL3.dll copy "SDL3-win\lib\x86\SDL3.dll" .
+    a.exe cpu_instrs.gb
+)
