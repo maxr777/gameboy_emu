dfe3435414c7e58129d45e88a88f08e7535a003c
max <maxr777@proton.me>
2025-08-28 18:58:20 +0200

    Some initial work on MBC1 and 2 space indentation -> tab

--- gameboy.h
+++ gameboy.h
@@ -9,15 +9,15 @@
 
 #define ROM_BANK_0_ADDR 0x0000
 #define ROM_BANK_N_ADDR 0x4000
-#define VRAM_ADDR       0x8000
+#define VRAM_ADDR		0x8000
 #define EXTERN_RAM_ADDR 0xA000
-#define WRAM_0_ADDR     0xC000
-#define WRAM_N_ADDR     0xD000 // DMG = extension of WRAM_0, CGB = switchable bank 1-7
-#define ECHO_RAM_ADDR   0xE000 // Prohibited, mirror of both WRAMs
-#define OAM_ADDR        0xFE00
-#define INVAL_MEM_ADDR  0xFEA0 // Prohibited
-#define IO_REGS_ADDR    0xFF00
-#define HRAM_ADDR       0xFF80
+#define WRAM_0_ADDR		0xC000
+#define WRAM_N_ADDR		0xD000 // DMG = extension of WRAM_0, CGB = switchable bank 1-7
+#define ECHO_RAM_ADDR	0xE000 // Prohibited, mirror of both WRAMs
+#define OAM_ADDR		0xFE00
+#define INVAL_MEM_ADDR	0xFEA0 // Prohibited
+#define IO_REGS_ADDR	0xFF00
+#define HRAM_ADDR		0xFF80
 #define INT_ENABLE_ADDR 0xFFFF
 
 // ================ IO REGISTERS ================
@@ -27,55 +27,55 @@
 
 // serial transfer
 #define SERIAL_TRANSFER 0xFF01
-#define SERIAL_CONTROL  0xFF02
+#define SERIAL_CONTROL	0xFF02
 
 // timer and divider
 #define DIVIDER_CONTROL 0xFF04
-#define TIMER_COUNTER   0xFF05
-#define TIMER_MOD       0xFF06
-#define TIMER_CONTROL   0xFF07
+#define TIMER_COUNTER	0xFF05
+#define TIMER_MOD		0xFF06
+#define TIMER_CONTROL	0xFF07
 
 // interrputs
 #define INTERRUPT_FLAG 0xFF0F
 
 // audio
-#define CH1_SWEEP       0xFF10
-#define CH1_TIMER       0xFF11
-#define CH1_VOLUME      0xFF12
-#define CH1_PERIOD_LOW  0xFF13
+#define CH1_SWEEP		0xFF10
+#define CH1_TIMER		0xFF11
+#define CH1_VOLUME		0xFF12
+#define CH1_PERIOD_LOW	0xFF13
 #define CH1_PERIOD_HIGH 0xFF14
-#define CH2_TIMER       0xFF16
-#define CH2_VOLUME      0xFF17
-#define CH2_PERIOD_LOW  0xFF18
+#define CH2_TIMER		0xFF16
+#define CH2_VOLUME		0xFF17
+#define CH2_PERIOD_LOW	0xFF18
 #define CH2_PERIOD_HIGH 0xFF19
-#define CH3_DAC_ENABLE  0xFF1A
-#define CH3_LEN_TIMER   0xFF1B
-#define CH3_OUTPUT_LVL  0xFF1C
-#define CH3_PERIOD_LOW  0xFF1D
+#define CH3_DAC_ENABLE	0xFF1A
+#define CH3_LEN_TIMER	0xFF1B
+#define CH3_OUTPUT_LVL	0xFF1C
+#define CH3_PERIOD_LOW	0xFF1D
 #define CH3_PERIOD_HIGH 0xFF1E
-#define CH4_LEN_TIMER   0xFF20
-#define CH4_VOLUME      0xFF21
-#define CH4_FREQUENCY   0xFF22
-#define CH4_CONTROL     0xFF23
-#define MASTER_VOLUME   0xFF24
-#define SOUND_PANNING   0xFF25
-#define AUDIO_CONTROL   0xFF26
+#define CH4_LEN_TIMER	0xFF20
+#define CH4_VOLUME		0xFF21
+#define CH4_FREQUENCY	0xFF22
+#define CH4_CONTROL		0xFF23
+#define MASTER_VOLUME	0xFF24
+#define SOUND_PANNING	0xFF25
+#define AUDIO_CONTROL	0xFF26
 
 // wave pattern
 #define WAVE_PATTERN_RAM 0xFF30
 
 // lcd control, status, position, scrolling, palettes
-#define LCD_CONTROL       0xFF40
-#define LCD_STATUS        0xFF41
-#define BG_VIEWPORT_Y     0xFF42
-#define BG_VIEWPORT_X     0xFF43
-#define LCD_Y             0xFF44
-#define LCD_LY_CMP        0xFF45
-#define BG_PALETTE_DATA   0xFF47
+#define LCD_CONTROL		  0xFF40
+#define LCD_STATUS		  0xFF41
+#define BG_VIEWPORT_Y	  0xFF42
+#define BG_VIEWPORT_X	  0xFF43
+#define LCD_Y			  0xFF44
+#define LCD_LY_CMP		  0xFF45
+#define BG_PALETTE_DATA	  0xFF47
 #define OBJ_PALETTE0_DATA 0xFF48
 #define OBJ_PALETTE1_DATA 0xFF49
-#define WINDOW_Y7         0xFF4A
-#define WINDOW_X7         0xFF4B
+#define WINDOW_Y7		  0xFF4A
+#define WINDOW_X7		  0xFF4B
 
 // vram bank select
 #define VRAM_BANK 0xFF4F
@@ -88,11 +88,11 @@
 #define VRAM_DMA_SRC_LOW   0xFF52
 #define VRAM_DMA_DEST_HIGH 0xFF53
 #define VRAM_DMA_DEST_LOW  0xFF54
-#define VRAM_DMA_START     0xFF55
+#define VRAM_DMA_START	   0xFF55
 
 // bg/obj palettes
-#define CGB_BG_PALETTE_SPEC        0xFF68
-#define CGB_BG_PALETTE_DATA        0xFF69
+#define CGB_BG_PALETTE_SPEC		   0xFF68
+#define CGB_BG_PALETTE_DATA		   0xFF69
 #define CGB_OBJ_COLOR_PALETTE_SPEC 0xFF6A
 #define CGB_OBJ_COLOR_PALETTE_DATA 0xFF6B
 
@@ -102,32 +102,32 @@
 // ================ HARDWARE ================
 
 enum RegisterNames {
-  AF,
-  BC,
-  DE,
-  HL,
-  SP, // always accessed as 16-bits
-  PC, // always accessed as 16-bits
-  REGISTER_COUNT
+	AF,
+	BC,
+	DE,
+	HL,
+	SP, // always accessed as 16-bits
+	PC, // always accessed as 16-bits
+	REGISTER_COUNT
 };
 
 enum Flags {
-  Z, // zero flag
-  N, // subtract flag
-  H, // half carry flag
-  C, // carry flag
-  FLAG_COUNT
+	Z, // zero flag
+	N, // subtract flag
+	H, // half carry flag
+	C, // carry flag
+	FLAG_COUNT
 };
 
 // BC = Full 16 bit register
 // B = High 8 bits
 // C = Low 8 bits
 typedef union {
-  uint16_t full;
-  struct {
-    uint8_t low;
-    uint8_t high;
-  };
+	uint16_t full;
+	struct {
+		uint8_t low;
+		uint8_t high;
+	};
 } Register;
 
 extern uint8_t ram[8192];
@@ -137,11 +137,11 @@ extern uint8_t oam[160];
 extern bool display[160][144];
 
 typedef struct {
-  Register regs[REGISTER_COUNT];
-  int cycle;
-  bool prefix;
-  bool ime;
-  int ime_enable_counter;
+	Register regs[REGISTER_COUNT];
+	int cycle;
+	bool prefix;
+	bool ime;
+	int ime_enable_counter;
 } CPU;
 
 extern CPU cpu;
@@ -149,39 +149,48 @@ extern CPU cpu;
 // ================ ROM STUFF ================
 
 typedef struct {
-  uint8_t entry_point[4];
-  uint8_t nintendo_logo[48];
-  char title[16]; // already includes manufacturer code
-  uint8_t cgb_flag;
-  uint16_t new_license_code;
-  uint8_t sgb_flag;
-  uint8_t cartridge_type;
-  uint8_t rom_size;
-  uint8_t ram_size;
-  uint8_t destination_code;
-  uint8_t old_licensee_code;
-  uint8_t mask_rom_version_number;
-  uint8_t header_checksum;
-  uint16_t global_checksum;
+	uint8_t entry_point[4];
+	uint8_t nintendo_logo[48];
+	char title[16]; // already includes manufacturer code
+	uint8_t cgb_flag;
+	uint16_t new_license_code;
+	uint8_t sgb_flag;
+	uint8_t cartridge_type;
+	uint8_t rom_size;
+	uint8_t ram_size;
+	uint8_t destination_code;
+	uint8_t old_licensee_code;
+	uint8_t mask_rom_version_number;
+	uint8_t header_checksum;
+	uint16_t global_checksum;
 } CartridgeHeader;
 
 typedef struct {
-  uint8_t *game_rom;
-  size_t game_size;
-  CartridgeHeader cartridge_header;
-  bool boot_rom_enabled;
-  const uint8_t boot_rom[256];
-  uint8_t current_bank;
-  int max_banks;
+	uint8_t *game_rom;
+	size_t game_size;
+	CartridgeHeader cartridge_header;
+	bool boot_rom_enabled;
+	const uint8_t boot_rom[256];
+	uint8_t current_bank;
+	int max_banks;
 } ROM;
 
 extern ROM rom;
 
+typedef struct {
+	bool ram_enable;
+	uint8_t first_rom_bank_reg;
+	uint8_t second_rom_bank_reg;
+	uint8_t ram_bank_number;
+} MBC1_State;
+
+extern MBC1_State mbc1;
+
 void rom_write(const uint16_t addr, const uint8_t val);
-void mcb1_write(const uint16_t addr, const uint8_t val);
+void mbc1_write(const uint16_t addr, const uint8_t val);
 
 uint8_t rom_read(const uint16_t addr);
-uint8_t mcb1_read(const uint16_t addr);
+uint8_t mbc1_read(const uint16_t addr);
 
 // ================ HELPER FUNCTIONS ================
 
